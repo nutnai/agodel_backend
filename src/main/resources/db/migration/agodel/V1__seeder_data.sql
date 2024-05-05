d6b79a1ac081349237aef964d1c093215c507dcb
nutnai <nutnai3302@gmail.com>
2024-05-05 17:02:43 +0700

    add username row in customer and owner table

--- src/main/resources/db/migration/agodel/V1__seeder_data.sql
+++ src/main/resources/db/migration/agodel/V1__seeder_data.sql
@@ -1,16 +1,34 @@
 -- Active: 1711037597965@@127.0.0.1@3306@agodel
 
 -- init user
-INSERT INTO user (id, username, password) 
-VALUES ('10000000', 'customer', 'password'), 
-        ('20000000', 'owner', 'password');
+INSERT INTO
+    user (id, username, password)
+VALUES (
+        '10000000', 'customer0', 'password'
+    ),
+    (
+        '20000000', 'owner0', 'password'
+    );
 -- init customer
-INSERT INTO customer (customer_id, firstname, lastname, phone, email)
-VALUES ('10000000', 'cfirstname0', 'clastname0', '1234567890', 'customer@email.com');
+INSERT INTO
+    customer (
+        customer_id, username, firstname, lastname, phone, email
+    )
+VALUES (
+        '10000000', 'customer0', 'cfirstname0', 'clastname0', '1234567890', 'customer@email.com'
+    );
 
 -- init owner
-INSERT INTO owner (owner_id, firstname, lastname, phone, email)
-VALUES ('20000000', 'ofirstname0', 'olastname0', '0987654321', 'owner@email.com');
+INSERT INTO
+    owner (
+        owner_id, username, firstname, lastname, phone, email
+    )
+VALUES (
+        '20000000', 'owner0', 'ofirstname0', 'olastname0', '0987654321', 'owner@email.com'
+    );
 
 -- set initial value user count
-INSERT INTO user_count (type, count) VALUES ('customer', 1), ('owner', 1);
+INSERT INTO
+    user_count (type, count)
+VALUES ('customer', 1),
+    ('owner', 1);
