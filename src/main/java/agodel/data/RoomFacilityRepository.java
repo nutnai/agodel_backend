d47608eff3850e967f89c7e5c9a7b8c42dac4d01
nutnai <nutnai3302@gmail.com>
2024-05-06 21:00:54 +0700

    Update RoomFacilityRepository.java

--- src/main/java/agodel/data/RoomFacilityRepository.java
+++ src/main/java/agodel/data/RoomFacilityRepository.java
@@ -3,7 +3,9 @@ package agodel.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import agodel.model.RoomModel;
+
 @Repository
-public interface RoomFacilityRepository extends JpaRepository<Integer, String> {
+public interface RoomFacilityRepository extends JpaRepository<RoomModel, String> {
 
 }
